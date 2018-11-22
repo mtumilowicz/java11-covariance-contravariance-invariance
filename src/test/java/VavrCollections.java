dc82375903d4c44abb7ee4b40e740f2ee5aa20f0
mtumilowicz <michal.tumilowicz01@gmail.com>
2018-11-22 20:43:49 +0100

    more example vavr

--- src/test/java/VavrCollections.java
+++ src/test/java/VavrCollections.java
@@ -13,19 +13,28 @@ public class VavrCollections {
     @Test
     public void hashSet() {
         HashSet<Dog> dogs = HashSet.of(new Dog(), new Dog());
-        HashSet<Animal> animals = HashSet.of(new Dog(), new Dog());
+        HashSet<Animal> animals = HashSet.ofAll(dogs);
+        HashSet<Animal> animals2 = HashSet.narrow(dogs);
+        
+        HashSet<Animal> animals3 = HashSet.of(new Dog(), new Dog());
     }
 
     @Test
     public void list() {
         List<Dog> dogs = List.of(new Dog(), new Dog());
-        List<Animal> animals = List.of(new Dog(), new Dog());
+        List<Animal> animals = List.ofAll(dogs);
+        List<Animal> animals2 = List.narrow(dogs);
+        
+        List<Animal> animals3 = List.of(new Dog(), new Dog());
     }
 
     @Test
     public void treeSet() {
         TreeSet<Dog> dogs = TreeSet.of(new Dog(), new Dog());
-        TreeSet<Animal> animals = TreeSet.of(new Dog(), new Dog());
+        TreeSet<Animal> animals = TreeSet.ofAll(dogs);
+        TreeSet<Animal> animals2 = TreeSet.narrow(dogs);
+        
+        TreeSet<Animal> animals3 = TreeSet.of(new Dog(), new Dog());
     }
 
     @Test
@@ -33,8 +42,11 @@ public class VavrCollections {
         HashMap<Integer, Dog> integerDogMap = HashMap.of(
                 1, new Dog(),
                 2, new Dog());
-        
-        HashMap<Number, Animal> numberAnimalMap = HashMap.of(
+
+//        HashMap.ofAll(integerDogMap); // only for java.util.map
+        HashMap<Number, Animal> numberAnimalMap2 = HashMap.narrow(integerDogMap);
+                
+        HashMap<Number, Animal> numberAnimalMap3 = HashMap.of(
                 1, new Dog(),
                 2, new Dog());
     }
