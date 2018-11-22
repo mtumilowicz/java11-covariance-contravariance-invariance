bc8fef0f6fb8e2dea318c985a934297605c36ef9
mtumilowicz <michal.tumilowicz01@gmail.com>
2018-11-22 20:52:31 +0100

    guava more examples

--- src/test/java/GuavaCollectionsTest.java
+++ src/test/java/GuavaCollectionsTest.java
@@ -6,6 +6,9 @@ import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.ImmutableSortedSet;
 import org.junit.Test;
 
+import java.util.Collections;
+import java.util.HashMap;
+
 /**
  * Created by mtumilowicz on 2018-11-22.
  */
@@ -15,18 +18,21 @@ public class GuavaCollectionsTest {
     public void immutableSet() {
         ImmutableSet<Dog> dogs = ImmutableSet.of(new Dog(), new Dog());
         ImmutableSet<Animal> animals = ImmutableSet.of(new Dog(), new Dog());
+        ImmutableSet<Animal> animals2 = ImmutableSet.copyOf(Collections.<Dog>emptySet());
     }
 
     @Test
     public void immutableList() {
         ImmutableList<Dog> dogs = ImmutableList.of(new Dog(), new Dog());
         ImmutableList<Animal> animals = ImmutableList.of(new Dog(), new Dog());
+        ImmutableList<Animal> animals2 = ImmutableList.copyOf(Collections.<Dog>emptyList());
     }
 
     @Test
     public void immutableSortedSet() {
         ImmutableSortedSet<Dog> dogs = ImmutableSortedSet.of(new Dog(), new Dog());
         ImmutableSortedSet<Animal> animals = ImmutableSortedSet.of(new Dog(), new Dog());
+        ImmutableSortedSet<Animal> animals2 = ImmutableSortedSet.copyOf(Collections.<Dog>emptyNavigableSet());
     }
 
     @Test
@@ -38,5 +44,7 @@ public class GuavaCollectionsTest {
         ImmutableMap<Number, Animal> numberAnimalMap = ImmutableMap.of(
                 1, new Dog(),
                 2, new Dog());
+
+        ImmutableMap<Number, Animal> numberAnimalMap2 = ImmutableMap.copyOf(new HashMap<Integer, Dog>());
     }
 }
